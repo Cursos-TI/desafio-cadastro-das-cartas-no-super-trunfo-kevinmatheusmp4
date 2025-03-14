1290abfe182b0bb790973965ff1affad8820d513
kevinmatheusmp4 <kevinmatheusquaresma@gmail.com>
2025-03-14 18:40:56 +0000

    Aqui está o desafio NOVATO do cadastro de Cartas Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main() { // função principal
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char estado[50], cidade[50], codigodacarta[50];
+    int populacao, PontosTuristicos;
+    float area, PIB;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Digite o nome do estado: \n");
+    scanf("%s", estado); 
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", cidade);
+
+    printf("Digite o número da populaçao desta cidade: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o número de pontos turisticos da cidade: \n");
+    scanf("%d", &PontosTuristicos);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", codigodacarta);
+
+    printf("Codigo da carta: %s\n", codigodacarta);
+    printf("\nEstado: %s - Cidade: %s\n", estado, cidade);
+    printf("Populaçao: %d - Area: %.2f\n", populacao, area);
+    printf("PIB: %.2f - Pontos Turisticos: %d\n", PIB, PontosTuristicos);
+
+    printf("\nPressione Enter para cadastrar outra carta...");
+    getchar(); // Pausa para o usuário pressionar Enter
 
     return 0;
+
 }
